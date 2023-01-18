c624ef550b51d15a41ee1df23b800ac10f2c76a9
tsukada <soranohoshi739@gmail.com>
2023-01-18 23:03:02 +0900

    bootstrap 利用

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,25 @@
 <template>
- Hello World
+ <div>
+   <Header />
+ </div>
 </template>
+
+<script>
+ import Header from './Header.Vue';
+
+ export default {
+  components: {
+    Header
+  }
+}
+</script>
+<style>
+#app {
+ font-family: Avenir, Helvetica, Arial, sans-serif;
+ -webkit-font-smoothing: antialiased;
+ -moz-osx-font-smoothing: grayscale;
+ text-align: center;
+ color: #2c3e50;
+ margin-top: 60px;
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import './bootstrap';
 import {createApp} from 'vue';
+import "bootstrap/dist/css/bootstrap.min.css"
 import App from './App.vue';
 createApp(App).mount("#app");
