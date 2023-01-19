cd265d03cb7ee6210ff43ca0bcb310c7c0b842e3
tsukada <soranohoshi739@gmail.com>
2023-01-19 21:15:21 +0900

    vue反映

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,25 +1,21 @@
+<script setup>
+import Hello from './components/Hello.vue';
+import Header from './components/Header/Header.vue';
+</script>
+
 <template>
- <div>
-   <Header />
- </div>
+<Header />
+<h1>Vue 3 入門</h1>
+<Hello />
 </template>
 
-<script>
- import Header from './Header.Vue';
-
- export default {
-  components: {
-    Header
-  }
-}
-</script>
 <style>
 #app {
- font-family: Avenir, Helvetica, Arial, sans-serif;
- -webkit-font-smoothing: antialiased;
- -moz-osx-font-smoothing: grayscale;
- text-align: center;
- color: #2c3e50;
- margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
