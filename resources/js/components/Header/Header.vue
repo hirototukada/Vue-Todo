82a118581f90e128470c444b28495db1cd2e89aa
tsukada <soranohoshi739@gmail.com>
2023-01-21 22:50:29 +0900

    ヘッダー基礎

--- resources/js/components/Header/Header.vue
+++ resources/js/components/Header/Header.vue
@@ -1,10 +1,15 @@
 <template>
  <nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand">
-        <router-link to="/home">ホーム</router-link>
-        <router-link to="/about">About</router-link>
-    </a>
-  </div>
+    <div class="w-100 d-flex justify-content-between">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+        <div class="container-fluid">
+            <a class="navbar-brand text-right w-100">
+                <router-link class="mr-3" to="/home">ホーム</router-link>
+                <router-link to="/about">About</router-link>
+            </a>
+        </div>
+    </div>
 </nav>
 </template>
