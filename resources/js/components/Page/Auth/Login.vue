5b46f3fedf1c0b1e332a59865a3d8da49181fe30
tsukada <soranohoshi739@gmail.com>
2023-02-08 23:56:23 +0900

    認証API

---
+++ resources/js/components/Page/Auth/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <h2>ログイン</h2>
+        <form class="login-form">
+            <div class="input-group">
+                <label for="email">メールアドレス</label>
+                <input type="email" id="email" />
+            </div>
+            <div class="input-group">
+                <label for="password">パスワード</label>
+                <input type="password" id="password" />
+            </div>
+            <div class="input-group">
+                <button type="button">送信</button>
+            </div>
+        </form>
+    </div>
+    <div class="w-50 m-auto">
+        <router-link to="/signUp" class="m-auto">新規登録はこちら</router-link>
+    </div>
+</template>
+
+<style>
+.input-group {
+    margin: 5px;
+}
+</style>
