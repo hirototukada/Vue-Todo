4d55c8ad6526876c38e42391ae5424b2e57e7925
tsukada <soranohoshi739@gmail.com>
2023-03-31 14:09:39 +0900

    firebaseSDKインストール

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,5 +5,16 @@ import router from "./router";
 import App from "./App.vue";
 import store from "./store";
 
+router.beforeEach((to, from, next) => {
+    if (
+        to.matched.some((record) => record.meta.requiresAuth) &&
+        !Auth.loggedIn
+    ) {
+        next({ path: "/login", query: { redirect: to.fullPath } });
+    } else {
+        next();
+    }
+});
+
 const app = createApp(App).use(router, store);
 app.mount("#app");
